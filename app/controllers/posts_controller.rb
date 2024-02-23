6ed4fbb2292b45133c5bc81ec39b170e79b4f750
dhav211 <dhav211@gmail.com>
2024-02-22 19:16:23 -0800

    Add submit new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,25 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.order('created_at DESC')
+    @new_post = Post.new
   end
 
   def show
     @post = Post.find params[:id]
     @comments = @post.comments.all
+  end
+
+  def create
+    current_user ||= User.find session[:user_id] if session[:user_id]
+    @new_post = Post.new allowed_params
+    @new_post.user = current_user
+
+    redirect_to @new_post if @new_post.save
+  end
+
+  private
+
+  def allowed_params
+    params.require(:post).permit(:title, :url, :user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create]
   resources :sessions, only: [:create]
   resources :users, only: [:create]
   delete 'signout', to: 'sessions#destroy'
